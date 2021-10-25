69d69381155f7567df48c42c9d065e114ac303fd
vincentsarsah <vincent.sarsah@amalitech.org>
2021-10-25 12:27:00 +0000

    shopping cart icon update

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function NavBar() {
-
-  const { cartTotalQuantity } = useSelector((state) => state.cart)
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
 
   return (
     <nav className="nav-bar">
@@ -18,11 +17,12 @@ function NavBar() {
             width="35"
             height="35"
             fill="currentColor"
-            className="bi bi-handbag-fill"
+            className="bi bi-cart3"
             viewBox="0 0 16 16"
           >
-            <path d="M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2zM5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0V5z" />
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
           </svg>
+         
           <span className="bag-quantity">
             <span>{cartTotalQuantity}</span>
           </span>
