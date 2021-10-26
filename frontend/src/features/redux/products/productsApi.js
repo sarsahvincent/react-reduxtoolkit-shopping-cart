7b88c3d80e52523200b7e2a074a95f1e0a2243ea
sarsahvincent <75670766+sarsahvincent@users.noreply.github.com>
2021-10-26 03:32:33 +0000

    baseUrl update

--- frontend/src/features/redux/products/productsApi.js
+++ frontend/src/features/redux/products/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://reactreduxshoppingcartbackend.herokuapp.com" }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
